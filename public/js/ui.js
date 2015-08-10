118d622c146ee6d017504b48f2f72d1562995c24
John Doe <andrey.roba@gmail.com>
2015-08-10 23:55:02 +0200

    add stub function for icon change on collapse category

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,3 +1,18 @@
 /**
  * Created by robaa on 02.07.2015.
  */
+/**
+ * Some links for collapse:
+ * http://stackoverflow.com/questions/18147338/twitter-bootstrap-3-0-icon-change-on-collapse
+ * http://getbootstrap.com/javascript/#collapse
+ * http://www.bootply.com/73101
+ */
+$(document).ready(function () {
+    $('.collapse')
+        .on('shown.bs.collapse', function () {
+            var obj = this;
+        })
+        .on('hidden.bs.collapse', function () {
+            var obj = this;
+        });
+});
