4b3d69c7b2686cb700847ee8a4dacdf2ae25c7ae
John Doe <andrey.roba@gmail.com>
2015-08-15 14:20:29 +0200

    enable hide of category navigation (todo fix home button css toggle)

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,6 +1,19 @@
 /**
  * Created by robaa on 02.07.2015.
+ *
+ * About event bubbling and how to get rid of it:
+ * http://stackoverflow.com/questions/12077859/difference-between-this-and-event-target
+ * Some links for collapse:
+ * http://stackoverflow.com/questions/18147338/twitter-bootstrap-3-0-icon-change-on-collapse
+ * http://getbootstrap.com/javascript/#collapse
+ * http://www.bootply.com/73101
  */
-$(document).ready(function () {
-
-});
+$('#sidebar-navigation')
+    .on('show.bs.collapse', function (e) {
+        if ($(e.target).hasClass('sidebar'))
+            $("#page-wrapper").css("margin-left", "250px");
+    })
+    .on('hide.bs.collapse', function (e) {
+        if ($(e.target).hasClass('sidebar'))
+            $("#page-wrapper").css("margin-left", "0px");
+    });
