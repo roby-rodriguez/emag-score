948d534eff717ed34daf06681a1f24e46f5764a5
John Doe <andrey.roba@gmail.com>
2015-08-19 21:17:55 +0300

    add stub with static data for home

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -2,5 +2,10 @@
  * Created by robaa on 11.08.2015.
  */
 angular.module('emagScoresApp').controller('HomeController', function($scope) {
-
+    $scope.tabs = [
+        { color : 'primary', icon : 'fa-smile-o', title : 'Trending Low' },
+        { color : 'green', icon : 'fa-frown-o', title : 'Trending High' },
+        { color : 'yellow', icon : 'fa-plus-circle', title : 'New Products' },
+        { color : 'red', icon : 'fa-ban', title : 'Retired products' },
+    ];
 });
