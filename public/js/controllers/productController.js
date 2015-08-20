916596b8da71860a8eb6a0db4ad9cf6a9f46a8a1
Roby Rodriguez <andrey.roba@gmail.com>
2015-08-20 18:53:32 +0300

    add back functionality to product detail view

--- public/js/controllers/productController.js
+++ public/js/controllers/productController.js
@@ -6,70 +6,73 @@
  *
  * Problems:
  * http://stackoverflow.com/questions/12618342/ng-model-does-not-update-controller-value
+ * http://stackoverflow.com/questions/18880737/how-do-i-use-rootscope-in-angular-to-store-variables
  * Each time a ng-controller is found, another $scope is created, which is why $rootScope is sometimes necessary
  * http://stackoverflow.com/questions/27770402/ng-repeat-not-updating-after-array-update#answer-27771130
  *
  * TODO controllers tend to clutter unnecessary business logic, refactor to utilities/services
  */
-angular.module('emagScoresApp').controller('ProductController', function($rootScope, $scope, $log, ProductFactory, ProductService, CategoryFactory) {
-    //TODO products doesn't have to be on root scope
-    $rootScope.products = [];
-    $rootScope.paginator = {
-        maxPages: 5,
-        currentPage: 1,
-        resultsPerPage: 5
-    };
+angular.module('emagScoresApp')
+    .run(function ($rootScope) {
+        //TODO products doesn't have to be on root scope
+        $rootScope.products = [];
+        $rootScope.paginator = {
+            maxPages: 5,
+            currentPage: 1,
+            resultsPerPage: 5
+        };
+    })
+    .controller('ProductController', function($rootScope, $scope, $log, ProductFactory, ProductService, CategoryFactory) {
+        $scope.pageChanged = function() {
+            $log.log("Changed to 2: " + $rootScope.paginator.currentPage);
+            $scope.displayProducts();
+        };
 
-    $scope.pageChanged = function() {
-        $log.log("Changed to 2: " + $scope.paginator.currentPage);
-        $scope.displayProducts();
-    };
+        $scope.ratingStyleClass = function(ratingScore, index) {
+            if (ratingScore >= (index + 1) * 20)
+                return 'fa fa-star';
+            else if (ratingScore > index * 20)
+                return 'fa fa-star-half-o';
+            else
+                return 'fa fa-star-o';
+        };
 
-    $scope.ratingStyleClass = function(ratingScore, index) {
-        if (ratingScore >= (index + 1) * 20)
-            return 'fa fa-star';
-        else if (ratingScore > index * 20)
-            return 'fa fa-star-half-o';
-        else
-            return 'fa fa-star-o';
-    };
+        $scope.setProduct = function(data) {
+            ProductFactory.setProduct(data);
+        };
 
-    $scope.setProduct = function(data) {
-        ProductFactory.setProduct(data);
-    };
+        $scope.emagBase = "http://www.emag.ro";
+        $scope.displayProducts = function () {
+            ProductService.retrieveProducts($scope.subcategory.name, $rootScope.paginator.currentPage, $rootScope.paginator.resultsPerPage)
+                .then(function (json) {
+                    // promise fulfilled
+                    $rootScope.products = json;
+                }, function(error) {
+                    // display error message in UI
+                });
+        };
+        $scope.retrieveTotalNrOfProducts = function () {
+            ProductService.retrieveTotalNrOfProducts($scope.subcategory.name)
+                .then(function (total) {
+                    // promise fulfilled
+                    $rootScope.paginator.total = total;
+                    $scope.displayProducts();
+                }, function(error) {
+                    // display error message in UI
+                });
+        };
 
-    $scope.emagBase = "http://www.emag.ro";
-    $rootScope.displayProducts = function () {
-        ProductService.retrieveProducts($scope.subcategory.name, $scope.paginator.currentPage, $scope.paginator.resultsPerPage)
-            .then(function (json) {
-                // promise fulfilled
-                $rootScope.products = json;
-            }, function(error) {
-                // display error message in UI
-            });
-    };
-    $scope.retrieveTotalNrOfProducts = function () {
-        ProductService.retrieveTotalNrOfProducts($scope.subcategory.name)
-            .then(function (total) {
-                // promise fulfilled
-                $rootScope.paginator.total = total;
-                $scope.displayProducts();
-            }, function(error) {
-                // display error message in UI
-            });
-    };
+        CategoryFactory.initCategory(function (data) {
+            $scope.subcategory = data;
+            $scope.retrieveTotalNrOfProducts();
+        });
 
-    CategoryFactory.initCategory(function (data) {
-        $scope.subcategory = data;
-        $scope.retrieveTotalNrOfProducts();
-    });
-
-    /**
-     * Listens to category changed events coming from category view
-     */
-    $scope.$on('categoryChanged', function () {
-        $scope.subcategory = CategoryFactory.getCategory();
-        $scope.displayProducts();
-        $scope.retrieveTotalNrOfProducts();
-    });
+        /**
+         * Listens to category changed events coming from category view
+         */
+        $scope.$on('categoryChanged', function () {
+            $scope.subcategory = CategoryFactory.getCategory();
+            $scope.displayProducts();
+            $scope.retrieveTotalNrOfProducts();
+        });
 });
