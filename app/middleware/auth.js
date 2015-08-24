3f16317c0a4fe2ddb5536773cbfc0ddc4e702fc8
John Doe <andrey.roba@gmail.com>
2015-08-24 23:24:58 +0300

    add logic for auth

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,3 +1,57 @@
 /**
  * Created by johndoe on 22.08.2015.
+ *
+ * Links:
+ * http://thejackalofjavascript.com/architecting-a-restful-node-js-app/
  */
+var jwt  = require('jwt-simple');
+var User = require('../model/User');
+
+var Auth = {
+    login: function (req, res) {
+        var email = req.body.email || '';
+        var password = req.body.password || '';
+
+        if (email == '' || password == '') {
+            res.status(401);
+            res.json({
+                "status": 401,
+                "message": "Invalid credentials"
+            });
+            return;
+        }
+
+        User.findByCredentials(email, password, function (user) {
+            if (!user) {
+                res.status(401);
+                res.json({
+                    "status": 401,
+                    "message": "Invalid credentials"
+                });
+                return;
+            } else {
+                res.json(generateToken(user));
+            }
+        });
+    }
+};
+
+function generateToken(user) {
+    var expires = expiresIn(7); // 7 days
+    var token = jwt.encode({
+        exp: expires
+    }, require('../config/secret')());
+
+    return {
+        token: token,
+        expires: expires,
+        user: user
+    };
+}
+
+function expiresIn(numDays) {
+    var dateObj = new Date();
+    return dateObj.setDate(dateObj.getDate() + numDays);
+}
+
+module.exports = Auth;
--- app/model/user.js
+++ app/model/user.js
@@ -1,3 +1,30 @@
 /**
+ * User document model.
+ *
  * Created by johndoe on 22.08.2015.
  */
+var Database  = require('./database');
+
+var User = {
+    findByCredentials: function (email, password, callback) {
+        console.log('findProductsByTitle: ' + req.params.title);
+        Database.connect().done(function (database) {
+                database.collection('user')
+                    .findOne({email: email, password: password}, function (err, doc) {
+                        if (err) {
+                            console.log('Error: ' + err);
+                        } else {
+                            console.log('found: ' + doc);
+                        }
+                        callback(doc);
+                    });
+            }, function (reason) {
+                // handle onRejected
+                // todo build custom error handler -> http://expressjs.com/guide/error-handling.html
+                console.log(reason);
+            }
+        );
+    }
+};
+
+module.exports = User;
