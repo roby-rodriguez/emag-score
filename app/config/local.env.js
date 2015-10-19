bf89614ec663301e36620ed4d7f528e4bf376f35
Roby Rodriguez <hello@youlookquitebeautifull.co>
2015-10-19 11:19:57 +0000

    add remote mongoDB property keys

--- app/config/local.env.js
+++ app/config/local.env.js
@@ -6,11 +6,17 @@
 module.exports = {
     /** todo Replace by appropriate env variables such as process.env.IP, PORT */
     DOMAIN: 'http://localhost:1337',
+    /** remote mongoDB username */
+    MONGO_USERNAME: '',
+    /** remote mongoDB password */
+    MONGO_PASSWORD: '',
+    /** remote mongoDB url */
+    MONGO_URL: '',
     /** base emag source url */
     EMAG_BASE_URL: 'http://www.emag.ro',
     /** emag recaptcha API public key - assume it doesn't change */
     EMAG_RECAPTCHA_PUBLIC_KEY: '6LdYnNESAAAAAJIiPTuRX5FjzwbUOPqz31ju7VTc',
-    /** recaptcha API source base URL */
+    /** recaptcha API source base URL - choose the no javascript version to bypass all the extra security */
     RECAPTCHA_CHALLENGE_BASE_URL: 'http://www.google.com/recaptcha/api/noscript?k=',
     /** recaptcha API source base URL */
     RECAPTCHA_IMAGE_BASE_URL: 'http://www.google.com/recaptcha/api/image?c=',
